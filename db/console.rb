c763cc2d76e87f7044159b314fd1aa4447b8f1c2
Graham Bruce <grahambruce84@gmail.com>
2017-06-04 17:59:41 +0100

    fixed some mistakes

--- db/console.rb
+++ db/console.rb
@@ -8,13 +8,11 @@ customer1 = Customer.new({
   'funds' => 30
   })
 customer1.save()
-
 customer2 = Customer.new({
   'name' => "Graham Bruce",
   'funds' => 20
   })
 customer2.save()
-
 customer3 = Customer.new({
   'name' => "Ryan Reynolds",
   'funds' => 50
@@ -26,7 +24,6 @@ film1 = Film.new({
   'price' => 5
   })
 film1.save()
-
 film2 = Film.new({
   'title' => "Pirates of the Carribbean 5",
   'price' => 5
@@ -37,11 +34,17 @@ ticket1 = Ticket.new({
   'customer_id' => customer1.id,
   'film_id' => film1.id
   })
-
+ticket1.save()
 ticket2 = Ticket.new({
   'customer_id' => customer2.id,
   'film_id' => film2.id
   })
+ticket2.save()
+ticket3 = Ticket.new({
+  'customer_id' => customer1.id,
+  'film_id' => film2.id
+  })
+ticket3.save()
 
 binding.pry
 nil
--- models/details.rb
+++ models/details.rb
@@ -3,8 +3,8 @@ class Details
   attr_accessor :name, :title
 
   def initialize(options)
-    @name = options('name')
-    @title = options('title')
+    @name = options['name']
+    @title = options['title']
   end
 
 end
